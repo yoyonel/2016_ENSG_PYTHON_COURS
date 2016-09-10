6829838c9a59c0eb60d4d43e3ad586fb5f7f1d30
Lionel ATTY <yoyonel@hotmail.com>
2016-09-11 01:37:37 +0200

    Rajout de docs dans les fichiers scripts.

--- travis-local/downgrade_coverage_for_py32.sh
+++ travis-local/downgrade_coverage_for_py32.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# url: https://github.com/travis-ci/travis-ci/issues/4866
+# 
 source ~/2016_ENSG_PYTHON_COURS/.tox/py32/bin/activate
 
 pip uninstall coverage
 
 pip install 'coverage<4'
 
+# url: https://virtualenv.pypa.io/en/stable/userguide/
 deactivate
--- travis-local/run_with_bash.sh
+++ travis-local/run_with_bash.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# urls: 
+# - https://docs.docker.com/engine/tutorials/dockervolumes/
+# - https://docs.docker.com/engine/reference/commandline/volume_inspect/
+# - https://docs.docker.com/engine/reference/commandline/volume_ls/
+# - https://forums.docker.com/t/volume-mounts-in-windows-does-not-work/10693/5
 docker	run -it --rm \
 		--name docker_travis_tox \
 		-v /d/__DATAS__/__DEV__/OSGEO4W/__COURS__/.tox/py26:/home/travis/2016_ENSG_PYTHON_COURS/.tox/py26 \
