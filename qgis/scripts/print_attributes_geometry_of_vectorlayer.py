11beaa7f3a6d05ff1dc8c96a220866db2c20990d
Lionel Atty <yoyonel@hotmail.com>
2016-09-26 23:44:12 +0200

    Refactoring du script 'print_attributes_geometry_of_vectorlayer.py' => Rajout de méthodes/functions pour décomposer les taches (les rendre plus unitaires, moins en mode monolythe ^^). Travail sur la récupération des layers possèdant les fields requis ('id', 'position'). WIP.

--- qgis/scripts/print_attributes_geometry_of_vectorlayer.py
+++ qgis/scripts/print_attributes_geometry_of_vectorlayer.py
@@ -1,6 +1,6 @@
 import os
 import glob
-from PyQt4.QtGui import QColor
+from PyQt4.QtGui import QColor, QLabel, QPixmap
 import random, sys
 
 # urls: 
@@ -44,6 +44,57 @@ def init_label_with_img(id_img):
     #
     return label
 
+def get_vector_layers_with_fields_0(layers, list_fields):
+    list_layers_found = []
+    for layer in layers:
+        fields_names = map(QgsField.name, layer.pendingFields())
+        if all([field in fields_names for field in list_fields]):
+            list_layers_found.append(layer)
+    return list_layers_found
+
+def get_vector_layers_with_fields_1a(layers, list_fields):
+    list_layers_found = []
+    for layer in layers:
+        fields_names = map(QgsField.name, layer.pendingFields())
+        list_layers_found.append((layer, all([field in fields_names for field in list_fields])))
+    #
+    def _func_tup(_tup):
+        return tup[1]
+    return filter(_func_tup, list_layers_found)
+
+def get_vector_layers_with_fields_1b(layers, list_fields):
+    list_layers_found = []
+    for layer in layers:
+        fields_names = map(QgsField.name, layer.pendingFields())
+        list_layers_found.append((layer, all([field in fields_names for field in list_fields])))
+    return filter(lambda tup: tup[1], list_layers_found)
+
+def get_vector_layers_with_fields_2a(layers, list_fields):
+    list_layers_with_matching_results = [ (layer, 
+                                all([field in map(QgsField.name, layer.pendingFields()) for field in list_fields]))
+                                for layer in layers 
+                            ]
+    return map(lambda tup: tup[0], filter(lambda tup: tup[1], list_layers_with_matching_results))
+
+def get_vector_layers_with_fields_2b(layers, list_fields):
+    return map(
+                lambda tup: tup[0], 
+                filter(
+                        lambda tup: tup[1], 
+                        [
+                            (
+                                layer, 
+                                all([
+                                        field in map(QgsField.name, layer.pendingFields())
+                                        for field in list_fields
+                                    ]
+                                )
+                            )
+                            for layer in layers
+                        ]
+                    )
+            )
+
 # local settings
 DIR_IMG = u'D:\\__DATAS__\\__DEV__\\OSGEO4W\\2016-07-18_LI3DS_Nexus5_Synch_BatU\\img'
 PATTERN_IMG = u'*.jpg'
@@ -59,41 +110,44 @@ except:
 else:
     print("%d images found in: %s" % (len(list_img_dirs), search_path_for_img))
 
+    # layers = get_vector_layers_with_attributes(['id', 'position'])
     layers = iface.mapCanvas().layers()
 
-    for layer in layers:
-        layerType = layer.type()
-        if layerType == QgsMapLayer.VectorLayer:
-            # url: http://gis.stackexchange.com/questions/76364/how-to-get-field-names-in-pyqgis-2-0
-            field_names = [field.name() for field in layer.pendingFields() ]
-            if u'id' in field_names and u'position' in field_names:
+    # for layer in layers:
+    #     layerType = layer.type()
+    #     if layerType == QgsMapLayer.VectorLayer:
+    #         # url: http://gis.stackexchange.com/questions/76364/how-to-get-field-names-in-pyqgis-2-0
+    #         field_names = [field.name() for field in layer.pendingFields() ]
+    #         if u'id' in field_names and u'position' in field_names:
+    layers_compatibles = get_vector_layers_with_fields_2a(layers, [u'id', u'position'])
+    for layer in layers_compatibles:
+        iter = layer.getFeatures()
+        
+        for feature in iter:
+            # fetch attributes
+            attrs = feature.attributes()
+            id_img = attrs[0]
 
-                iter = layer.getFeatures()
-                
-                for feature in iter:
-                    # fetch attributes
-                    attrs = feature.attributes()
-                    id_img = attrs[0]
+            if bi_contains(list_img_basename, id_img):
+                #
+                id_img_in_list = bisect_left(list_img_basename, id_img)
+                dir_path_img = list_img_dirs[id_img_in_list]
+                fullpath_to_img = os.path.join(dir_path_img, id_img)
+                #
+                #print("Fullpath to image: %s" % (fullpath_to_img))
 
-                    if bi_contains(list_img_basename, id_img):
-                        #
-                        id_img_in_list = bisect_left(list_img_basename, id_img)
-                        dir_path_img = list_img_dirs[id_img_in_list]
-                        fullpath_to_img = os.path.join(dir_path_img, id_img)
-                        #
-                        #print("Fullpath to image: %s" % (fullpath_to_img))
+                # retrieve every feature with its geometry and attributes
+                # fetch geometry
+                geom = feature.geometry()
+                qgis_feature_id = feature.id()
+                #print "Feature ID %d: " % qgis_feature_id
 
-                        # retrieve every feature with its geometry and attributes
-                        # fetch geometry
-                        geom = feature.geometry()
-                        qgis_feature_id = feature.id()
-                        #print "Feature ID %d: " % qgis_feature_id
+                # show some information about the feature
+                if geom.type() == QGis.Point:
+                    position = geom.asPoint()
+                    # update dict images
+                    dict_imgs.update(build_img_dict(id_img, dir_path_img, qgis_feature_id, position))
 
-                        # show some information about the feature
-                        if geom.type() == QGis.Point:
-                            position = geom.asPoint()
-                            # update dict images
-                            dict_imgs.update(build_img_dict(id_img, dir_path_img, qgis_feature_id, position))
     try:
         id_img = list_img_basename[get_random_id(list_img_basename)]
         feature_id_selected = get_feature_id(id_img)
@@ -110,6 +164,9 @@ else:
         print("Feature selected: %d" % (feature_id_selected))
         #
         label = init_label_with_img(id_img)
-        label.show()
+        try:
+            label.show()
+        except Exception as err:
+            print("Unexpected error:", err)   
 finally:
     print("Nb images synch with QGIS: %d" % (len(dict_imgs)))
