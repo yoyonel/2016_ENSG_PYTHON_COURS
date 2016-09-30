45ae5d5fe33241c8202911ae9ce19b62238c8d54
ATTY Lionel <yoyonel@hotmail.com>
2016-10-01 01:45:47 +0200

    Fix pour le nouveau path des données (en submodule). Rajout d'un fichier TODO.txt

--- scripts/launch_OriExport_to_shapefile_Linux.sh
+++ scripts/launch_OriExport_to_shapefile_Linux.sh
@@ -2,7 +2,7 @@
 mkdir -p export/
 
 python app/OriExport_to_shapefile.py \
-    ../2016-07-18_LI3DS_Nexus5_Synch_BatU/micmac/OriExport_Ori-RTL-Init.txt \
+    data-2016-07-18_LI3DS_Nexus5_Synch_BatU/micmac/OriExport_Ori-RTL-Init.txt \
     -v \
     -s export/OriExport_Ori-RTL-Init \
     $@
