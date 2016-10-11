69c03ed30505ccfde3cc080190e6e102d8d34fe7
yoyonel <yoyonel@hotmail.com>
2016-10-12 00:05:49 +0200

    Modification du script de lancement 'scripts/launch_OriExport_to_shapefile_Linux.sh' rajoute une variable sur le path du fichier OriExport à prendre en compte. C'est encore un peu cracra faut cleaner. W.I.P.

--- scripts/launch_OriExport_to_shapefile_Linux.sh
+++ scripts/launch_OriExport_to_shapefile_Linux.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 mkdir -p export/
 
+ORIEXPORT=datas/2016-10-11_ProtoLI3DS_BatU/2016-10-11_OriExport_Ori-RTL-Init.txt
+#ORIEXPORT=datas/2016-07-18_LI3DS_Nexus5_Synch_BatU/micmac/OriExport_Ori-RTL-Init.txt
+
 python app/OriExport_to_shapefile.py \
-    data-2016-07-18_LI3DS_Nexus5_Synch_BatU/micmac/OriExport_Ori-RTL-Init.txt \
+    $ORIEXPORT \
     -v \
     -s export/OriExport_Ori-RTL-Init \
     $@
